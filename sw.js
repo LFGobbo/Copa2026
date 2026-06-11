8edb08ecb88a327504874f20b1d546c1cf6fae29
LFGobbo <lfgobbo@users.noreply.github.com>
2026-06-11 15:20:41 -0300

    SW v4: network-first for HTML (always fresh), cache-only for assets, bypass stale HTML cache

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-var C='copa2026-v3';
-var FILES=['./','./index.html','./bola_t.png','./mascote1_t.png','./mascote2_t.png','./mascote3_t.png','./logo_globo.png','./logo_sportv.png','./logo_cazetv.png','./logo_sbt.png','./logo_nsports.png'];
-self.addEventListener('install',function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(FILES)}).then(function(){return self.skipWaiting()}))});
+var C='copa2026-v4';
+var ASSETS=['./bola_t.png','./mascote1_t.png','./mascote2_t.png','./mascote3_t.png','./logo_globo.png','./logo_sportv.png','./logo_cazetv.png','./logo_sbt.png','./logo_nsports.png'];
+self.addEventListener('install',function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(ASSETS.concat(['./index.html']))}).then(function(){return self.skipWaiting()}))});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==C}).map(function(k){return caches.delete(k)}))}).then(function(){return self.clients.claim()}))});
-self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}).catch(function(){return caches.match('./index.html')}))});
+self.addEventListener('fetch',function(e){
+  var url=e.request.url;
+  if(ASSETS.some(function(a){return url.endsWith(a)}))e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}));
+  else e.respondWith(fetch(e.request)['catch'](function(){return caches.match('./index.html')}));
+});
