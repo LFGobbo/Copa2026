9b464da18bc2e8ab84d5b8c6ec155bc1342bc57e
LFGobbo <lfgobbo@users.noreply.github.com>
2026-06-11 15:14:07 -0300

    Debounce renderSquads, add URL hash + localStorage tab persistence, SW v3, debug logs

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var C='copa2026-v2';
-var FILES=['./','./index.html','./copa2026.html','./bola_t.png','./mascote1_t.png','./mascote2_t.png','./mascote3_t.png','./logo_globo.png','./logo_sportv.png','./logo_cazetv.png','./logo_sbt.png','./logo_nsports.png'];
+var C='copa2026-v3';
+var FILES=['./','./index.html','./bola_t.png','./mascote1_t.png','./mascote2_t.png','./mascote3_t.png','./logo_globo.png','./logo_sportv.png','./logo_cazetv.png','./logo_sbt.png','./logo_nsports.png'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(FILES)}).then(function(){return self.skipWaiting()}))});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==C}).map(function(k){return caches.delete(k)}))}).then(function(){return self.clients.claim()}))});
 self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}).catch(function(){return caches.match('./index.html')}))});
