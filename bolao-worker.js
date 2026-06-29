76963430b40bec6b1113de56641bb4aa2d58a258
LFGobbo <lfgobbo@github.com>
2026-06-29 13:27:43 -0300

    Heitor 100 pts

--- bolao-worker.js
+++ bolao-worker.js
@@ -221,7 +221,7 @@ async function handle(req) {
 
     // GET /ranking
     if (method === 'GET' && path === '/ranking') {
-      var participants = (await supaFetch('participants?select=id,name,confirmed,confirmed_at')) || [];
+      var participants = (await supaFetch('participants?select=id,name,confirmed,confirmed_at,bonus_points')) || [];
       var maxGame = url.searchParams.get('maxGame');
       var picksUrl = 'picks?select=participant_id,game_n,goals_a,goals_b,ko_pick';
       if (maxGame) picksUrl += '&game_n=lte.' + encodeURIComponent(maxGame);
